26286ce75dbbde2e0d7315a941f9d82c7ca0e2fa
Waseem Akhtar <waseem9622640@gmail.com>
2026-05-22 01:36:44 +0500

    Updated sequential chain

--- 2_sequential_chain.py
+++ 2_sequential_chain.py
@@ -2,6 +2,9 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import os
+
+os.environ['langchain_project'] = 'sequential app'
 
 load_dotenv()
 
@@ -15,11 +18,18 @@ prompt2 = PromptTemplate(
     input_variables=['text']
 )
 
-model = ChatOpenAI()
+model = ChatOpenAI(model='gpt-4o-mini', temperature=0.5)
+model2 = ChatOpenAI(model='gpt-4o', temperature=0.7)
 
 parser = StrOutputParser()
 
-chain = prompt1 | model | parser | prompt2 | model | parser
+chain = prompt1 | model | parser | prompt2 | model2 | parser
+
+config = {
+    'run_name': 'sequential chain',
+    'tags': ['llm app', 'report_genration', 'summrization'],
+    'metadata': {'model':'gpt-4o-mini'}
+}
 
 result = chain.invoke({'topic': 'Unemployment in India'})
 
